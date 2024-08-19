f2efa7f7a0147736bff783b9e1374c7182052bdd
MarcoSac1 <marcosaccone8@gmail.com>
2024-08-19 15:27:30 +0200

    fixed dot issue

--- database/migrations/2024_08_06_151117_update_author_column_id_to_author_id_to_post_table--table=posts.php
+++ database/migrations/2024_08_06_151117_update_author_column_id_to_author_id_to_post_table--table=posts.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('posts'. function (Blueprint $table){
+        Schema::table('posts', function (Blueprint $table){
             $table->unsignedBigInteger('author')->change();
             $table->renameColumn('author','user_id');
         });
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('posts'. function (Blueprint $table){
+        Schema::table('posts', function (Blueprint $table){
             $table->renameColumn('user_id','author');
             $table->string('author')->change();
         });
